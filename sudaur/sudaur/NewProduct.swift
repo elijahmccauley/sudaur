2670754885e76e0661aa626d1ac9552686422f28
elijahmccauley <emmccauley6@gmail.com>
2025-01-06 23:57:48 -0500

    Make product creation page connect to firebase

--- sudaur/sudaur/NewProduct.swift
+++ sudaur/sudaur/NewProduct.swift
@@ -12,14 +12,63 @@ import FirebaseFirestore
 
 struct NewProduct: View {
     let db = Firestore.firestore()
-    let category = ["Nutrition","Recovery","Apparel","Other"]
+    let categories = ["Nutrition","Recovery","Apparel","Other"]
     @State private var brand = ""
     @State private var product = ""
-    @State private var amount = 0
+    @State private var amount = ""
     @State private var selectedCategory = "Nutrition"
+    @State private var errorMessage = ""
 
     var body: some View {
         Text("Add a New Product")
+        TextField("Brand", text: $brand)
+            .padding()
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .autocapitalization(.none)
+        TextField("Product", text: $product)
+            .padding()
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .autocapitalization(.none)
+        TextField("Amount", text: $amount)
+            .padding()
+            .textFieldStyle(RoundedBorderTextFieldStyle())
+            .autocapitalization(.none)
+        Picker("Category", selection: $selectedCategory) {
+            ForEach(categories, id: \.self) { category in Text(category).tag(category)
+            }
+        }
+        .pickerStyle(MenuPickerStyle())
+        .padding()
+        .border(Color.black)
+        if !errorMessage.isEmpty {
+            Text(errorMessage)
+                .foregroundColor(.red)
+                .padding()
+        }
+        
+        Button(action: {
+            createProduct(brand: brand, product: product, amount: amount, category: selectedCategory)
+        }) {
+            Text("Create")
+                .padding()
+                .background(Color.green)
+                .foregroundColor(.white)
+                .cornerRadius(8)
+        }
+    }
+    func createProduct(brand: String, product: String, amount: String, category: String) {
+        db.collection("products").document().setData([
+            "brand": brand,
+            "product": product,
+            "category": category,
+            "amount": amount
+            ]) { error in
+            if let error = error {
+                print("Error writing document: \(error)")
+            } else {
+                print("Document successfully written!")
+            }
+        }
     }
 }
 
