ac252d30016a538b7263f6b7585b894b4871185a
elijahmccauley <emmccauley6@gmail.com>
2025-01-28 00:04:19 -0500

    Add tile flipping ability
    Kinda sorta, the words are now backwards and theyre a different shape

--- sudaur/sudaur/TileView.swift
+++ sudaur/sudaur/TileView.swift
@@ -9,24 +9,46 @@ import SwiftUI
 
 struct TileView: View {
     let product: Product
+    @State private var isFlipped = false
     var body: some View {
-        VStack {
-            Image("leon")
-            .resizable()
-            .aspectRatio(contentMode: .fit)
-            Text(product.brand as String)
-                .font(.headline)
-                .multilineTextAlignment(.center)
-            
-            Text(product.amount as String)
-                .font(.headline)
-                .multilineTextAlignment(.center)
-            //    .padding([.bottom], 20)
-            //    .font(.system(size: 20))
+        ZStack {
+            if !isFlipped {
+                VStack {
+                    Image("leon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    Text(product.brand as String)
+                        .font(.headline)
+                        .multilineTextAlignment(.center)
+                    
+                    Text(product.amount as String)
+                        .font(.headline)
+                        .multilineTextAlignment(.center)
+                    //    .padding([.bottom], 20)
+                    //    .font(.system(size: 20))
+                }
+                .background(Color.gray)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+            }
+            if isFlipped {
+                VStack {
+                    Text(product.description as String)
+                        .font(.headline)
+                        .multilineTextAlignment(.center)
+                        .padding()
+                }
+                .background(Color.gray)
+                .cornerRadius(10)
+                .shadow(radius: 5)
+            }
         }
-        .background(Color.gray)
-        .cornerRadius(10)
-        .shadow(radius: 5)
+        .rotation3DEffect(.degrees(isFlipped ? 180 : 0),
+                          axis: (x: 0, y: 1, z: 0))
+        .animation(.easeInOut(duration: 0.6), value: isFlipped)
+        .onTapGesture {
+            isFlipped.toggle()
+        }
     }
 }
 
